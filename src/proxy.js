d6877a08df23809cadff56dda0d6c914061024d8
ajmainhossain7 <ajmainh0@gmail.com>
2026-05-03 00:50:14 +0600

    Proxy customize

--- src/proxy.js
+++ src/proxy.js
@@ -1,22 +1,19 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+// import { auth } from "./lib/auth";
 
-export async function proxy(request) {
-  const sessionToken =
-    request.cookies.get("better-auth.session_token")?.value ||
-    request.cookies.get("__Secure-better-auth.session_token")?.value;
+// export async function proxy(request) {
+//   const session = await auth.api.getSession({
+//     headers: await headers(),
+//   });
 
-  console.log("🔥 proxy running:", request.nextUrl.pathname);
-  console.log("🍪 token:", sessionToken);
+//   if (!sessionToken) {
+//     return NextResponse.redirect(new URL("/signin", request.url));
+//   }
 
-  if (!sessionToken) {
-    console.log("❌ no session → redirecting to signin");
-    return NextResponse.redirect(new URL("/signin", request.url));
-  }
+//   console.log("✅ session found → allow");
+//   return NextResponse.next();
+// }
 
-  console.log("✅ session found → allow");
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/profile", "/all-books/:path*"],
-};
+// export const config = {
+//   matcher: ["/profile", "/all-books/:path"],
+// };
